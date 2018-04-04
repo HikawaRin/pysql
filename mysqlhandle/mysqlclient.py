61069109e7bb9c39759ab27c49ea3ec3be1c356a
releaseavalon <releaseavalon@163.com>
2018-04-04 16:22:53 +0800

    add operations, change file name

--- mysqlhandle/mysqlclient.py
+++ mysqlhandle/mysqlclient.py
@@ -1,4 +1,4 @@
-
+#mysql for test ip:118.24.5.150 user:admin pass:admin
 import pymysql
 
 class mysql:
@@ -13,14 +13,33 @@ class mysql:
         if self.database is not None:
             self.database.close()
 
-    def Database(self, database):
-        self.database = pymysql.connect(self.host, self.user, self.password, database)
-        if self.database is None:
-            print("connect database fail")
+    def SAVE(self):
+        if self.database is not None:
+            self.database.commit()
         else:
-            print("Database changed to:", database);
+            print("No useable database")
+            
+    def SHOW_Databases(self):
+        databases = pymysql.connect(self.host, self.user, self.password)
+        if databases.open:
+            cursor = databases.cursor()
+            cursor.execute("SHOW DATABASES")
+            data = cursor.fetchall()
+            databases.close()
+            cursor.close()
+            for item in data:
+                print(item)
+        else:
+            print("connect database fail")
+        
+    def Database(self, database = "test2"):
+        self.database = pymysql.connect(self.host, self.user, self.password, database)
+        if self.database.open:
+            print("Database changed to:", database)
+        else:
+            print("connect database fail")
 
-    def SELECT(self, column_name = "*", table_name = " ", where = None):
+    def SELECT(self, column_name = "*", table_name = "test", where = None):
         if self.database is None:
             print("No useable database")
             return
@@ -31,9 +50,51 @@ class mysql:
                 path = path + " WHERE " + where
             cursor.execute(path)
             data = cursor.fetchall()
+            cursor.close()
             for item in data:
                 print(item)
 
+    def INSERT(self, table_name = "test"):
+        if self.database is None:
+            print("No useable database")
+            return
+        else:
+            title = self.database.cursor()
+            st = "DESC " + table_name
+            title.execute(st)
+            td = title.fetchall()
+            title.close()
+            for item in td:
+                print(item)
+            s = input("VALUES:")
+            s = "INSERT INTO " + table_name + " VALUES(" + s + ")"
+            print(s)
+            cursor = self.database.cursor()
+            cursor.execute(s)
+            cursor.close()
+
+    def DELETE(self, table_name = "test", where = " "):
+        print("Are you sure del:")
+        mysql.SELECT(self, table_name = table_name, where = where)
+        x = input("(y/n)")
+        s = "DELETE FROM " + table_name + " WHERE " + where
+        if x == "y":
+            mysql.Execute(self, s)
+            print("Use save to del from database")
+
+    def UPDATE(self, table_name = "test", Set = " ", where = None)
+
+    def Execute(self, s):
+        if self.database is None:
+            print("No useable database")
+            return
+        else:
+            cursor = self.database.cursor()
+            cursor.execute(s)
+            #data = cursor.fetcall()
+            #if data is not None:
+            #    for item in data:
+            #        print(item)
 
             
             
